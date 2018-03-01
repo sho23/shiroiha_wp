c8e0942e1753bd0bb771d843740439269ef03233
sho23 <testuser.shota@gmail.com>
2018-03-01 12:49:08 +0900

    住所の出力を修正

--- wp-content/themes/gensen_tcd050/single-introduce.php
+++ wp-content/themes/gensen_tcd050/single-introduce.php
@@ -104,7 +104,7 @@ $dp_options = get_desing_plus_option();
     </div>
     <div class="pull-right">
       <ul>
-        <li class="btn btn-blue elm-inline"><a href="<?php echo $elms[3]; ?>"><p class="btn-icon"><i class="fas fa-map-marker-alt fa-2x"></i></p><p>MAP</p></a></li>
+        <li class="btn btn-blue elm-inline"><a href="<?php echo $elms[3]; ?>" target="_blank"><p class="btn-icon"><i class="fas fa-map-marker-alt fa-2x"></i></p><p>MAP</p></a></li>
         <li class="btn btn-blue elm-inline"><a href="#test-popup" class="open-popup-link"><p class="btn-icon"><i class="fas fa-calendar-alt fa-2x"></i></p><p>予約</p></a></li>
         <div id="test-popup" class="white-popup mfp-hide">
           <div class="pop-img"><img src="<?php echo get_template_directory_uri(); ?>//img/common/imgmodal.jpg" alt=""></div>
